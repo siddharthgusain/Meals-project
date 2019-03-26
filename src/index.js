2d5942e8a04afe86e14ff44117827b094b2339ec
siddharth gusain <siddharthgusain26011999@gmail.com>
2019-03-26 20:05:57 +0530

    created redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducers';
+import { createStore } from 'redux';
+
+const store = createStore(reducer);
+console.log(store);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
