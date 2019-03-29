a7e452c278e2e08ca0e2ede41caccca79ca2edc4
siddharth gusain <siddharthgusain26011999@gmail.com>
2019-03-30 02:12:29 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-
-
+import { connect } from 'react-redux';
+import calendar from '../reducers';
 class App extends Component {
+
+  
   render() {
+    console.log(this.props);
     return (
       <div className="App">
        Hello world
@@ -11,4 +14,23 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(calender){
+  const dayOrder=['sunday','monday','tuesday','wednesday','thrusday','friday','saturday'];
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+        ? calendar[day][meal]
+        : null
+        return meals
+      }, {})
+    })),
+  }
+}
+
+
+export default connect(
+  mapStateToProps,
+)(App);
